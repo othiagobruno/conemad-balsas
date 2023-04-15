abcbe89499b0304d53056269b36a300a8b3b5fad
Thiago Bruno <thiagobrunomiranda@gmail.com>
2023-04-15 09:49:53 -0300

    feature: social login

---
+++ src/pages/api/modules/auth/services/social-login.service.ts
@@ -0,0 +1,43 @@
+import {
+  FirebaseService,
+  LoginCredentials,
+} from '@/pages/api/providers/firebase/firebase.service'
+import { PrismaService } from '@/pages/api/providers/prisma/prisma.service'
+import jwt from 'jsonwebtoken'
+
+export class SocialLoginService {
+  private prisma = PrismaService
+  private firebase = new FirebaseService()
+
+  async execute(data: LoginCredentials) {
+    const user = await this.firebase.signInWithCredential(data)
+
+    let userExists = await this.prisma.user.findFirst({
+      where: { auth_provider_uid: user.auth_provider_uid },
+    })
+
+    if (!userExists) {
+      userExists = await this.prisma.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar: user.avatar,
+          auth_provider: user.auth_provider,
+          auth_provider_uid: user.auth_provider_uid,
+          password: '',
+        },
+      })
+    }
+
+    const access_token = jwt.sign(
+      { id: userExists.id, name: userExists.name },
+      process.env.JWT_SECRET,
+      { expiresIn: '1y' }
+    )
+
+    return {
+      access_token,
+      user: userExists,
+    }
+  }
+}
---
+++ src/pages/api/social-login.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { IAuth } from './modules/auth/interfaces/model/auth.model'
+import { SocialLoginService } from './modules/auth/services/social-login.service'
+import { LoginCredentials } from './providers/firebase/firebase.service'
+
+const socialLoginService = new SocialLoginService()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<
+    | IAuth
+    | {
+        error: string
+      }
+  >
+) {
+  if (req.method === 'POST') {
+    try {
+      const data = req.body as LoginCredentials
+      const user = await socialLoginService.execute(data)
+      res.status(201).json(user)
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(400).json({ error: error.message })
+      }
+    }
+  }
+}
