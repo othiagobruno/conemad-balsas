a2c8931ddc773edbeabecb80810034bbe4203a6d
Thiago Bruno <thiagobrunomiranda@gmail.com>
2023-04-17 09:14:24 -0300

    feat: add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { themeApp } from '@/theme'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
+import GoogleTagManager from '@/shared/GoogleTagManager'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CacheProvider>
       <ChakraProvider theme={themeApp}>
+        <GoogleTagManager />
         <GoogleAnalytics trackPageViews strategy="lazyOnload" />
         <Component {...pageProps} />
       </ChakraProvider>
