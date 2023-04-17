578d5a83cd7a6b7ef6d86c77a42a6e872f8b36c3
Thiago Bruno <thiagobrunomiranda@gmail.com>
2023-04-17 09:04:47 -0300

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { themeApp } from '@/theme'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CacheProvider>
       <ChakraProvider theme={themeApp}>
+        <GoogleAnalytics trackPageViews strategy="lazyOnload" />
         <Component {...pageProps} />
       </ChakraProvider>
     </CacheProvider>
